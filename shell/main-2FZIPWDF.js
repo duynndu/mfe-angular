eada0c59e52f6c67a12d0044fee087992007ca01
duynndu <duynndu@users.noreply.github.com>
2025-11-08 11:56:08 +0000

    deploy: d647b90194bcafad1788c7cf0c30146eeb465442

---
+++ shell/main-2FZIPWDF.js
@@ -0,0 +1,1 @@
+import{b as r,d as e}from"./chunk-WEUJGB4W.js";console.log(e);r(e.federationRemotes).catch(o=>console.error(o)).then(o=>import("./chunk-SGTRMKUP.js")).catch(o=>console.error(o));
--- shell/main-QHXCUSUA.js
+++
@@ -1,1 +0,0 @@
-import{b as r,d as e}from"./chunk-WEUJGB4W.js";console.log(e);r(e.federationRemotes).catch(o=>console.error(o)).then(o=>import("./chunk-WOYYPFDV.js")).catch(o=>console.error(o));
