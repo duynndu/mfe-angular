e6f46d1c5a4ac774131c82480c8eee6091ef1cc4
duynndu <duynndu@users.noreply.github.com>
2026-08-28 16:25:20 +0000

    deploy: 3a89767f80afd4b74b2a29e87e9b4c6941d727d3

--- main-65PGZJBU.js
+++
@@ -1,1 +0,0 @@
-import{c as n}from"./chunk-DRMXH472.js";var e={production:!1};console.log(e);n("federation.manifest.json").catch(o=>console.error(o)).then(o=>import("./chunk-DBVRC35W.js")).catch(o=>console.error(o));
